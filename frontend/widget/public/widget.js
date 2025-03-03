ac574a2910608d42f6cf4cadd528689d6ddd213a
Kittithat Jaijang <Kittithat.cg@gmail.com>
2025-03-03 10:32:41 +0700

    Update widget.js

--- frontend/widget/public/widget.js
+++ frontend/widget/public/widget.js
@@ -1,7 +1,7 @@
 (function() {
   const script = document.createElement("script");
 
-  //กำหนดเองว่าต้องโหลดจากที่ไหน (แก้ตรงนี้ได้เลย)
+  //กำหนดเองว่าต้องโหลดจากที่ไหน
   script.src = "http://localhost:3000/widget-bundle.js";  // ใช้ Localhost
 
   document.body.appendChild(script);
