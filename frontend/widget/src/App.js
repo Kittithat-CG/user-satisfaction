111e5083a1c677f7d40d58db9fcf0b5abc2803f9
Kittithat Jaijang <Kittithat.cg@gmail.com>
2025-03-03 10:33:26 +0700

    Update App.js

--- frontend/widget/src/App.js
+++ frontend/widget/src/App.js
@@ -7,7 +7,7 @@ const FeedbackWidget = () => {
   const [comment, setComment] = useState("");
 
   const handleSubmit = async () => {
-    await axios.post("https://user-satisfaction.onrender.com/feedback", {
+    await axios.post("http://localhost:3000/feedback", {
       user_id: "12345",
       app_name: "Test App",
       rating,
