850e6f896d9955311eacf8a07131fba439eb8130
Kittithat Jaijang <Kittithat.cg@gmail.com>
2025-03-03 09:56:27 +0700

    Update test

--- frontend/widget/public/widget.js
+++ frontend/widget/public/widget.js
@@ -2,7 +2,7 @@
   const script = document.createElement("script");
 
   //กำหนดเองว่าต้องโหลดจากที่ไหน (แก้ตรงนี้ได้เลย)
-  script.src = "https://user-satisfaction.onrender.com";
+  script.src = "https://user-satisfaction-widget.onrender.com";
   // script.src = "http://localhost:3000/widget-bundle.js";  // ใช้ Localhost
   // script.src = "https://beverest.thaibev.com/satisfaction/widget-bundle.js";  // ใช้ Production
 
